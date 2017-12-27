12de821cf6dc5f54db087f8a50eb3ea18ddb1a5d
wangmutian66 <824495596@qq.com>
2017-12-27 19:04:21 +0800

    12.27.v2

---
+++ BuDeJie/Classes/FriendTrend(关注)/View/WMTLoginRegister.h
@@ -0,0 +1,13 @@
+//
+//  WMTLoginRegister.h
+//  BuDeJie
+//
+//  Created by wangmutian on 2017/12/27.
+//  Copyright © 2017年 wangmutian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WMTLoginRegister : UIView
+
+@end
---
+++ BuDeJie/Classes/FriendTrend(关注)/View/WMTLoginRegister.m
@@ -0,0 +1,21 @@
+//
+//  WMTLoginRegister.m
+//  BuDeJie
+//
+//  Created by wangmutian on 2017/12/27.
+//  Copyright © 2017年 wangmutian. All rights reserved.
+//
+
+#import "WMTLoginRegister.h"
+
+@implementation WMTLoginRegister
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
